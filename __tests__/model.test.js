da156b3353d7776eda32484ba83182923b807bfc
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-21 21:25:55 -0800

    jsdocs, fixed parsing error, successfully ran lint

--- __tests__/model.test.js
+++ __tests__/model.test.js
@@ -6,14 +6,13 @@ const Model = require('../models/mongo-model');
 const model = new Model();
 
 describe('CRUD functionality', () => {
-  
   it('can create a record', async () => {
     let obj = { name: 'teacups', count: 12 };
     let createObj = await model.create(obj);
     Object.keys(obj).forEach(key => {
-      expect(createObj[key]).toEqual(obj)[key]);
+      expect(createObj[key]).toEqual(obj)[key];
+    });
   });
-});
 
   it('can get a record', async () => {
     let obj = { name: 'plates', count: 6 };
@@ -21,5 +20,4 @@ describe('CRUD functionality', () => {
     let fetchObj = await model.get(createObj._id);
     expect(fetchObj._id).toEqual(createObj._id);
   });
-
-  
+});
